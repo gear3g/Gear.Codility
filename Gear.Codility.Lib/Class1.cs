4f1d428f8a1b046edb11cd9e5fc06577b29b53e8
Gustavo Granados <granados.gg@gmail.com>
2018-08-31 20:04:22 -0500

    Adding ILesson interface

--- Gear.Codility.Lib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Gear.Codility.Lib
-{
-    public class Class1
-    {
-    }
-}
---
+++ Gear.Codility.Lib/ILesson.cs
@@ -0,0 +1,7 @@
+﻿namespace Gear.Codility.Lib {
+
+    public interface ILesson<TOutput, TInput> {
+
+        TOutput solution(TInput input);
+    }
+}
