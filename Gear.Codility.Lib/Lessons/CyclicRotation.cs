cccf5f1d7c60ae5e202e027a79ce71c2f1a80a1d
Gustavo Granados <granados.gg@gmail.com>
2018-08-31 20:10:03 -0500

    Adding CyclicRotation

---
+++ Gear.Codility.Lib/Lessons/CyclicRotation.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Gear.Codility.Lib.Lessons {
+
+    /// <summary>
+    /// https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+    /// </summary>
+    /// <typeparam name="int[]"></typeparam>
+    /// <typeparam name="CyclicRotationArgument"></typeparam>
+    public class CyclicRotation : ILesson<int[], CyclicRotationArgument>
+    {
+        public int[] solution(CyclicRotationArgument input)
+        {
+            var A = input.A;
+            var K = input.K;
+
+            var n = A.Length;
+            var result = new int[n];
+            K = n > 0 ? K % n : 0;
+
+            for (int i = 0; i < n; i++) {
+                var position = i - K;
+                if (position < 0) position += n;
+
+                result[i] = A[position];
+            }
+
+            return result;
+        }
+    }
+
+    public class CyclicRotationArgument {
+
+        public int[] A { get; set; }
+
+        public int K { get; set; }
+    }
+}
